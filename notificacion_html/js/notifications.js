0c209473325cad3d6ccf4b7e247d79fa2cce0d03
Antonio Pol <pol370@gmail.com>
2015-08-17 10:47:27 -0700

    update settings example

--- notificacion_html/js/notifications.js
+++ notificacion_html/js/notifications.js
@@ -1,17 +1,17 @@
 var Notifications = function (newSettings) {
   var settings = {
-        position: "top-right",
-        backgroundColor : "bg-black",
-        color: "bg-white",
-        animation: "slide-in-right",
-        image: false,
-        removable: true,
-        srcImage: "http://api.ning.com/files/Dqlf20RAm4vk*NYNkBsByZjS7xZp4x2ZwDwqH2N9NMx161P-qV*2nJxYE2RbF7HJK5BUtGOxGL2zEhtyKLO2N7Yd2wV2uum7/13z4l881.gif",
-        time: 3000,
-        canUrl: false,
-        url: "http://www.google.com",
-      },
-      area = document.getElementById("notifications");
+      position: "top-right",
+      backgroundColor : "bg-black",
+      color: "white",
+      animation: "slide-in-right",
+      image: true,
+      removable: true,
+      srcImage: "http://api.ning.com/files/Dqlf20RAm4vk*NYNkBsByZjS7xZp4x2ZwDwqH2N9NMx161P-qV*2nJxYE2RbF7HJK5BUtGOxGL2zEhtyKLO2N7Yd2wV2uum7/13z4l881.gif",
+      time: 3000,
+      canUrl: false,
+      url: "https://github.com/antoniopol06/angular-notifications",
+    },
+    area = document.getElementById("notifications");
 
   var set = function(newSettings){
     for (var property in newSettings){
